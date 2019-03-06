9b11c3d8af49859a4d429bbc3056e15f0372378e
Isabel <isabel.antonia.seidl@gmail.com>
2019-03-06 11:04:10 +0100

    improved styling

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
           <v-app>
            
 
-          <v-toolbar class="blue lighten-5">
-              <v-toolbar-side-icon @click="sideNav = true" class="hidden-sm-and-up"></v-toolbar-side-icon>
+          <v-toolbar dark class="blue lighten-5">
+              <v-toolbar-side-icon color='orange' @click="sideNav = true" class="hidden-sm-and-up"></v-toolbar-side-icon>
 
-              <v-toolbar-title>Menu</v-toolbar-title>
+              <v-toolbar-title style='color: orange'>Menu</v-toolbar-title>
               <v-spacer></v-spacer>
               <v-toolbar-items class="hidden-xs-only">
                 <v-btn flat  color="orange" to="/login">Login</v-btn>
@@ -21,28 +21,28 @@
              <v-list>
 
                  <v-list-tile-title class="m-3 mt-4 mb-4">
-                     <v-icon class="mr-2">account_box</v-icon>
+                     <v-icon color='orange' class="mr-2">account_box</v-icon>
                      <router-link to="/login">Login</router-link>
                  </v-list-tile-title>
                  <v-divider></v-divider>
 
 
                  <v-list-tile-title class="m-3 mt-4 mb-4">
-                     <v-icon class="mr-2">account_circle</v-icon>
+                     <v-icon color='orange' class="mr-2">account_circle</v-icon>
                      <router-link to="/signup">Signup</router-link>
                  </v-list-tile-title>
                  <v-divider></v-divider>
 
 
                   <v-list-tile-title class="m-3 mt-4 mb-4">
-                     <v-icon class="mr-2">schedule</v-icon>
+                     <v-icon color='orange' class="mr-2">schedule</v-icon>
                      <router-link to="/schedule">Schedule</router-link>
                  </v-list-tile-title>
                  <v-divider></v-divider>
 
 
                   <v-list-tile-title class="m-3 mt-4 mb-4">
-                     <v-icon class="mr-2">chat_bubble</v-icon>
+                     <v-icon color='orange' class="mr-2">chat_bubble</v-icon>
                      <router-link to="/chat">Chat</router-link>
                  </v-list-tile-title>
                  <v-divider></v-divider>
--- src/views/Matchdetails.vue
+++ src/views/Matchdetails.vue
@@ -1,9 +1,9 @@
 <template>      
    <div style='height: 100%; display: flex; align-items: center; justify-content: center' class="matchdetails">
    
-                  <v-card style="height: 480px; width: 300px; margin-bottom: 55px; padding: 5px; display: flex; flex-direction: column; align-items: center; justify-content: center; background-color: orange">
+                  <v-card style="height: 480px; width: 300px; margin-bottom: 55px; padding: 5px; display: flex; flex-direction: column; align-items: center; justify-content: center;">
 
-                       <div style="text-align: center;" id="myGrid">
+                       <div style="text-align: center; color: orange" id="myGrid">
                       <p id="t1name">{{CurrentMatch.t1name}}</p>
                       <p id="t2name">{{CurrentMatch.t2name}}</p>
                       <p id="vs">vs.</p>
@@ -12,10 +12,10 @@
                       <p id='time'>{{CurrentMatch.time}}</p>
                       <p id='where'>Where:</p>
                       <p id='field'>{{CurrentMatch.field}}</p>
-                      <p style='font-size: 8px' id='direction'>{{CurrentMatch.fieldDirecction}}</p>
+                      <p style='font-size: 10px' id='direction'>{{CurrentMatch.fieldDirecction}}</p>
 </div>
 
-<p style="text-align: center; padding: 10px; border: 3px solid white">Please note that smoking is not permitted during any of our matches.</p>
+<p style="text-align: center; padding: 10px; border: 3px solid orange; color: orange">Please note that smoking is not permitted during any of our matches.</p>
                       </v-card>
           
       </div>
@@ -106,7 +106,7 @@ grid-column-start: 3;
 
 
 p {
-    font-size: 10px;
+    font-size: 15px;
 }
 
 </style>
