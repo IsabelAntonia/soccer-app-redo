36cc5673ddc6cac16ae35c90f8f75b2b31332481
Isabel <isabel.antonia.seidl@gmail.com>
2019-03-08 17:44:34 +0100

    chat works fine

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,45 +1,47 @@
 <template>
-    <div style="height: 100%; display: flex; align-items: center; justify-content: center; padding: 0" class="about">
-<v-card style="height: 480px; width: 300px; margin-bottom: 55px; overflow: scroll">
-   <img style="height: auto; width: 100%;" src="@/assets/kids.jpeg" alt="image_children"> 
-          
-       
+   <div style="height: 100%; display: flex; align-items: center; justify-content: center; padding: 0" class="about">
+      <v-card style="height: 480px; width: 300px; margin-bottom: 55px; overflow: scroll">
+         <img style="height: auto; width: 100%;" src="@/assets/kids.jpeg" alt="image_children">
 
-<v-card-text style="padding: 20px">
-     <h1>Our Mission:</h1>
-        <p>We want to support young athletes living in Chicago´s northside neighborhoods, who have an interest in learning and playing soccer.
-           We provide our members with the opportunitiy to learn an practice skills related to the game of soccer, specifically those skills around team cooperation and good sportsmanship.</p>
 
-        <h1>Our Vision:</h1>
-        <p>The Northside Youth Soccer League aspires to develop strong, well-rounded, and mindful athletes through the building of character, self-discipline, and leadership.</p>
 
-        <h1>General information:</h1>
-        <p>The Northside Youth Soccer League was established in 1996 to provide athletes residing in Chicago’s northside neighborhoods an enviroment in which to learn and play soccer. 
-        To be a member of NYSL, you must be between the ages of 4-12 and reside in a Chicago northside neighbourhood. 
-        NYSL is ran by a small full-time staff, and relies on the generous volunteer time of parents and previous league members.</p>
-  
-</v-card-text>
+         <v-card-text style="padding: 20px">
+            <h1>Our Mission:</h1>
+            <p>We want to support young athletes living in Chicago´s northside neighborhoods, who have an interest in
+               learning and playing soccer.
+               We provide our members with the opportunitiy to learn an practice skills related to the game of soccer,
+               specifically those skills around team cooperation and good sportsmanship.</p>
 
-   </v-card> 
-     </div>
+            <h1>Our Vision:</h1>
+            <p>The Northside Youth Soccer League aspires to develop strong, well-rounded, and mindful athletes through
+               the building of character, self-discipline, and leadership.</p>
+
+            <h1>General information:</h1>
+            <p>The Northside Youth Soccer League was established in 1996 to provide athletes residing in Chicago’s
+               northside neighborhoods an enviroment in which to learn and play soccer.
+               To be a member of NYSL, you must be between the ages of 4-12 and reside in a Chicago northside
+               neighbourhood.
+               NYSL is ran by a small full-time staff, and relies on the generous volunteer time of parents and
+               previous league members.</p>
+
+         </v-card-text>
+
+      </v-card>
+   </div>
 </template>
 
 <script>
-export default {
+   export default {
 
-}
+   }
 </script>
 
 <style scoped>
+   h1 {
+      font-size: 18px;
+      text-align: center;
+      margin-bottom: 20px;
+      margin-top: 40px;
 
-h1 {
-    font-size: 18px;
-    text-align: center;
-    margin-bottom: 20px;
-    margin-top: 40px;
-   
-}
-
-
-
+   }
 </style>
--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -2,139 +2,151 @@
 
 
 
-<div class="chat" style='height: 100%;'>
+    <div class="chat" style='height: 100%;'>
 
-   <div id="scrollDiv" style='display: flex; flex-direction: column; align-items: flex-start; overflow: scroll;'>
+        <div id="scrollDiv" style='display: flex; flex-direction: column; align-items: flex-start; overflow: scroll;'>
 
-    <div v-for="(key,i) in messages" :key="i" class="bubble" :class="{right: key.name === loggedInUser}">
-          <!-- the presence of the right class will be determined by whether key.name is the loggedInUser -->
-           <small v-if="key.name === loggedInUser">You</small>
-           <small v-if="key.name !== loggedInUser">{{key.name}}</small>
-            <p>{{key.message}}</p>
+            <div v-for="(key,i) in messages" :key="i" class="bubble" :class="{right: key.name === loggedInUser}">
+                <!-- the presence of the right class will be determined by whether key.name is the loggedInUser -->
+                <small v-if="key.name === loggedInUser">You</small>
+                <small v-if="key.name !== loggedInUser">{{key.name}}</small>
+                <p>{{key.message}}</p>
+            </div>
         </div>
-</div>
 
 
 
 
 
-   <div class="fixed-bottom" style='height: 60px; width: 100%; background-color: white; display: flex; align-items: center'>
-        <input id='textInput' style='border: 3px solid orange; width: 75%; height: 40px; margin-left: 10px; border-radius: 20px; padding: 10px; overflow: scroll' placeholder="Write your message" type="text">
-        <v-icon large color='blue' style='margin-left: 20px;' @click="sendMessagesToFirebase" >send</v-icon>
+        <div class="fixed-bottom" style='height: 60px; width: 100%; background-color: white; display: flex; align-items: center'>
+            <textarea id='textInput' cols="5" rows="1"></textarea>
+
+
+            <v-icon large color='blue' style='margin-left: 20px;' @click="sendMessagesToFirebase">send</v-icon>
 
         </div>
-   
-</div>
-    
+
+    </div>
+
 </template>
 
 <script>
- import firebase from 'firebase'
-export default {
+    import firebase from 'firebase'
+    export default {
 
-    data(){
+        data() {
 
-        return {
-            messages: {},
-            loggedInUser: {},
-           
-           
-
-        }
-    },
-
-   mounted(){
- this.calculateHeight();
-   },
+            return {
+                messages: {},
+                loggedInUser: {},
 
 
-    created(){
-        this.getMessagesFromFirebase();
-     
-       
 
-    },
+            }
+        },
 
-    updated(){
-   document.getElementById('scrollDiv').scrollTop = document.getElementById('scrollDiv').scrollHeight;
-
-    },
+        mounted() {
+            this.calculateHeight();
+        },
 
 
-    methods: {
-          calculateHeight(){
+        created() {
+            this.getMessagesFromFirebase();
 
-var screenHeight = window.screen.height;
 
-var calculatedHeight = screenHeight - (56 + 60);
 
-calculatedHeight = calculatedHeight + 'px'
+        },
 
-         document.querySelector('#scrollDiv').style.height = calculatedHeight;
-          
-         
-         },
+        updated() {
+            document.getElementById('scrollDiv').scrollTop = document.getElementById('scrollDiv').scrollHeight;
 
-    
-              sendMessagesToFirebase() {
+        },
+
+
+        methods: {
+            calculateHeight() {
+
+                var screenHeight = window.screen.height;
+
+                var calculatedHeight = screenHeight - (56 + 60);
+
+                calculatedHeight = calculatedHeight + 'px'
+
+                document.querySelector('#scrollDiv').style.height = calculatedHeight;
+
+
+            },
+
+
+            sendMessagesToFirebase() {
 
                 let text = document.getElementById("textInput").value;
                 let message = {
                     message: text,
                     name: firebase.auth().currentUser.email
                 };
-               
+
 
                 firebase.database().ref('chat').push(message); //push the message to firebase
-                document.getElementById('textInput').value ="";//empty the text input field again
+                document.getElementById('textInput').value = " "; //empty the text input field again
+
 
             },
             getMessagesFromFirebase() {
-                
-                this.loggedInUser= firebase.auth().currentUser.email;
+
+                this.loggedInUser = firebase.auth().currentUser.email;
 
                 firebase.database().ref('chat').on('value', (data) => {
                     this.messages = data.val();
-                 
-                 
+
+
                 })
             }
 
 
-    }
-  
+        }
 
-}
+
+    }
 </script>
 
 <style scoped>
+    .bubble {
 
-.bubble 
-{
+        margin: 10px;
+        display: inline-block !important;
+        max-width: 75vw;
+        height: auto;
+        text-align: center;
+        padding: 14px;
+        background: #FFFFFF;
+        border-radius: 10px;
+        ;
+    }
 
-margin: 10px;
-display: inline-block !important;
-max-width: 75vw;
-height: auto;
-text-align:center;
-word-break: break-all;
-padding: 14px;
-background: #FFFFFF;
-border-radius: 10px;;
-}
-
- .right {
+    .right {
         background-color: #E3F2FD;
         align-self: flex-end;
-        
-       } 
+
+    }
 
 
-       p {
+    p {
 
-           margin-bottom: 0;
-       }
-   
+        margin-bottom: 0;
+    }
+
+
+    #textInput {
+
+        border: 3px solid orange;
+        width: 75vw;
+        height: 40px;
+        margin-left: 10px;
+        border-radius: 20px;
+        padding: 10px;
+        overflow: scroll;
+
+
+    }
 </style>
-
- 
