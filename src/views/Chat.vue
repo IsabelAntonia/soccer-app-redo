ba5ccf124fb1136377861d6e9282874e165e4f58
Isabel <isabel.antonia.seidl@gmail.com>
2019-03-09 20:42:43 +0100

    added arrows to the speech bubbles

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -6,7 +6,7 @@
 
         <div id="scrollDiv" style='display: flex; flex-direction: column; align-items: flex-start; overflow: scroll;'>
 
-            <div v-for="(key,i) in messages" :key="i" class="bubble" :class="{right: key.name === loggedInUser}">
+            <div v-for="(key,i) in messages" :key="i" class="bubble" :class="{right: key.name === loggedInUser, left: key.name !== loggedInUser }">
                 <!-- the presence of the right class will be determined by whether key.name is the loggedInUser -->
                 <small v-if="key.name === loggedInUser">You</small>
                 <small v-if="key.name !== loggedInUser">{{key.name}}</small>
@@ -47,6 +47,7 @@
 
         mounted() {
             this.calculateHeight();
+          
         },
 
 
@@ -76,6 +77,7 @@
 
 
             },
+
 
 
             sendMessagesToFirebase() {
@@ -111,29 +113,12 @@
 </script>
 
 <style scoped>
-    .bubble {
-
-        margin: 10px;
-        display: inline-block !important;
-        max-width: 75vw;
-        height: auto;
-        text-align: center;
-        padding: 14px;
-        background: #FFFFFF;
-        border-radius: 10px;
-        ;
-    }
-
-    .right {
-        background-color: #E3F2FD;
-        align-self: flex-end;
-
-    }
-
+  
 
     p {
 
         margin-bottom: 0;
+        font-size: 12px;
     }
 
 
@@ -149,4 +134,65 @@
 
 
     }
+
+
+    .bubble {
+
+        margin-top: 10px;
+        margin-bottom: 10px;
+        display: inline-block !important;
+        max-width: 75vw;
+        height: auto;
+        text-align: center;
+        padding: 14px;
+        background: #FFFFFF;
+        border-radius: 10px;
+        position: relative;
+        word-break: break-word;
+
+    }
+
+     .left:after {
+        content: '';
+        position: absolute;
+        border-style: solid;
+        border-width: 10px 10px 10px 0;
+        border-color: transparent #FFFFFF;
+        display: block;
+        width: 0;
+       
+        z-index: 1;
+        left: -10px;
+        top: calc(50% - 10px);
+    }
+
+      .right:after {
+        content: '';
+        position: absolute;
+        border-style: solid;
+        border-width: 10px 0px 10px 10px;
+        border-color: transparent #E3F2FD;
+        display: block;
+        width: 0;
+        z-index: 1;
+        right: -10px;
+        top: calc(50% - 10px);
+    }
+
+
+      .right {
+       margin-right: 20px;
+        background-color: #E3F2FD;
+        align-self: flex-end;
+        
+    }
+
+    .left {
+ margin-left: 20px;
+ 
+
+    }
+
+ 
+
 </style>
